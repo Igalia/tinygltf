a83211097dacb55742f68b965564170818cf5c6f
Eduardo Lima <elima@filetea.me>
2023-05-05 10:21:53 +0200

    Add definition of `asset_manager` variable in implementation code when enabled

--- tiny_gltf.cc
+++ tiny_gltf.cc
@@ -2,3 +2,9 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "tiny_gltf.h"
+
+#ifdef __ANDROID__
+#ifdef TINYGLTF_ANDROID_LOAD_FROM_ASSETS
+AAssetManager *asset_manager = nullptr;
+#endif
+#endif
